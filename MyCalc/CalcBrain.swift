4d4d4c43646051f06890a865fa350e5d74735d0d
friedelio <friedel.arendt@gmx.net>
2016-09-15 21:03:43 +0200

    sample
    first steps in swift

---
+++ MyCalc/CalcBrain.swift
@@ -0,0 +1,79 @@
+//
+//  CalcBrain.swift
+//  MyCalc
+//
+//  Created by friedel arendt on 24.07.16.
+//  Copyright © 2016 friedel arendt. All rights reserved.
+//
+
+import Foundation
+
+
+class CalcBrain {
+    
+    private var accumulator: Double = 0.0
+    var OPisPending = false
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "sin" : Operation.UnaryOperation(sin),
+        "abs" : Operation.UnaryOperation(abs),
+        "+/-" : Operation.UnaryOperation({ -$0 }),
+        "*" : Operation.BinOperation({ $0 * $1 }),
+        "/" : Operation.BinOperation({ $0 / $1 }),
+        "-" : Operation.BinOperation({ $0 - $1 }),
+        "+" : Operation.BinOperation({ $0 + $1 }),
+
+        "=" : Operation.Equals,
+    ]
+    
+    
+    private func execPendingBinOp() {
+        if pending != nil {
+            accumulator = pending!.binFunc(pending!.firstOperand, accumulator)
+            pending = nil
+            OPisPending = false
+        }
+    }
+    
+    
+    private var pending: PendingBinOpInfo?
+    
+    private struct PendingBinOpInfo {
+        var binFunc: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double{get{return accumulator}}
+    
+    func setOperand	(operand: Double) {
+        accumulator = operand
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value): accumulator = value
+            case .UnaryOperation(let function): accumulator = function(accumulator)
+            case .BinOperation(let function):
+                OPisPending = true
+                execPendingBinOp()
+                pending = PendingBinOpInfo(binFunc: function, firstOperand: accumulator)
+                
+            case .Equals: execPendingBinOp()
+                
+            }
+        }
+    }
+    
+}
---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by friedel arendt on 24.07.16.
+//  Copyright © 2016 friedel arendt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet private weak var digLB: UILabel!
+    
+    @IBOutlet private weak var hist: UILabel!
+    
+    private var dispval: Double {
+        get { return Double(digLB.text!)!}
+        set {
+            //if String(newValue).rangeOfString(".") == nil
+            if String(Double(Int(newValue))) == String(Double(newValue))
+            {
+                digLB.text = String(Int(newValue))
+                
+            }
+            else
+            {
+                digLB.text = String(newValue)
+            }
+        }
+    }
+    
+    
+    private var cb = CalcBrain()
+    private var isUsrTyping = false
+    private var isTypingNK  = false
+    private var isNewval = true
+
+    
+    
+    @IBAction private func digitBT(sender: UIButton)
+    {
+        if let dt = sender.currentTitle
+        {
+            if isUsrTyping
+            {
+                if dt == "."
+                {
+                    if !isTypingNK
+                    {
+                        isTypingNK = true
+                        if digLB.text! != "."
+                        {
+                            digLB.text! += dt
+                            hist.text! += dt
+                        }
+                    }
+                }
+                else
+                {
+                    digLB.text! += dt
+                    hist.text! += dt
+                }
+            }
+            else
+            {
+                digLB.text! = dt
+                
+                if isNewval
+                {
+                    hist.text! = dt
+                    isNewval = false
+                }
+                else
+                {
+                    hist.text! += dt
+                }
+            }
+            isUsrTyping = true
+        }
+    }
+    
+    @IBAction func performOP(sender: UIButton) {
+        if isUsrTyping {
+            cb.setOperand(dispval)
+            isUsrTyping = false
+            isTypingNK  = false
+        }
+        if let mathsymbol = sender.currentTitle {
+            hist.text! += mathsymbol
+            cb.performOperation(mathsymbol)
+            if !cb.OPisPending && mathsymbol == "="
+            {
+                isNewval = true
+            }
+        }
+        dispval = cb.result
+    }
+}
+
